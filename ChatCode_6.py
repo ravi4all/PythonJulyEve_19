7f95aa856d1d73df1830c3f16b961c0f5d5fbf0d
ravi4all <ravikanttyagi.2295@gmail.com>
2019-07-22 18:22:49 +0530

    Add files via upload

--- ChatCode_6.py
+++ ChatCode_6.py
@@ -1,8 +1,12 @@
 from datetime import datetime as dt
+import os
+import glob, random
+import webbrowser
 
 helloIntent = ["hello","hi","hey","hey there","hi there","hello there"]
 dateIntent = ["date today","today's date","date please","tell me date","date"]
 timeIntent = ["tell me time","current time","please tell me time","time"]
+musicIntent = ["play music","play song","start song"]
 
 chat = True
 while chat:
@@ -16,6 +20,15 @@ while chat:
     elif msg in dateIntent:
         date = dt.now().date()
         print(date.strftime("%d/%m/%y,%a"))
+    elif msg in musicIntent:
+        path = "C:/Users/asus/Music"
+        os.chdir(path)
+        songs = glob.glob("*.mp3")
+        s = random.choice(songs)
+        os.startfile(s)
+    elif msg.startswith("open"):
+        web = msg.split()[-1]
+        webbrowser.open(web+'.com')
     elif msg == "bye":
         print("Bye User")
         chat = False
